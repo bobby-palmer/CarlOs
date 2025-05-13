9c52d1c7b21e5decc95356f8dd5f5c0350f1df7c
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-13 15:09:46 -0400

    fast

--- build.zig
+++ build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
     const kernel = b.addExecutable(.{
         .name = "kmain",
         .target = target,
-        .optimize = .ReleaseSmall,
+        .optimize = .ReleaseFast,
     });
 
     kernel.link_z_max_page_size = 4096;
