4ccfef9fe2c82d6e6d6d19bb4d35d49d722a20e0
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-13 10:21:50 -0400

    better bootloading

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.resolveTargetQuery(.{
-        .cpu_arch = .riscv32,
+        .cpu_arch = .riscv64,
         .os_tag = .freestanding,
         .abi = .none
     });
@@ -10,41 +10,40 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const kernel = b.addExecutable(.{
-        .name = "kernel.elf",
+        .name = "kmain",
         .target = target,
         .optimize = optimize,
     });
 
+    const cflags = &.{
+        "-ffreestanding", 
+        "-nostartfiles", 
+        "-nostdlib", 
+        "-nodefaultlibs", 
+        "-g", 
+        "-Wl,--gc-sections", 
+        "-mcmodel=medany", 
+    };
+
+
     kernel.addCSourceFiles(.{
         .files = &.{
-            "kernel/kmain.c"
+            "kernel/kmain.c",
+            "kernel/crt0.s",
         },
-        .flags = &.{
-            "-std=c11",
-            "-g3",
-            "-Wall",
-            "-Wextra",
-            "-fno-stack-protector",
-            "-ffreestanding",
-            "-nostdlib",
-        }
+        .flags = cflags
     });
 
-    kernel.entry = .{ .symbol_name =  "boot" };
     kernel.setLinkerScript(b.path("kernel/kernel.ld"));
 
     const qemu = b.step("qemu", "Run Qemu");
     const runQemu = b.addSystemCommand(&.{
-        "qemu-system-riscv32",
-        "-machine",
-        "virt",
-        "-bios",
-        "default",
-        "-nographic",
-        "-serial",
-        "mon:stdio",
-       "--no-reboot",
-        "-kernel",
+        "qemu-system-riscv64",
+        "-nographic", 
+        "-machine", "virt", 
+        "-m",       "128M",      
+        "-bios",    "none", 
+       "-kernel", 
     });
 
     runQemu.addArtifactArg(kernel);
--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,19 +1,4 @@
-extern char __bss[], __bss_end[], __stack_top[];
-
 void kmain(void) 
 {
   for (;;);
-}
-
-// Setup stack pointer and jump to kmain
-__attribute__((section(".text.boot")))
-__attribute__((naked))
-void boot(void) 
-{
-    __asm__ __volatile__(
-        "mv sp, %[stack_top]\n"
-        "j kmain\n"      
-        :
-        : [stack_top] "r" (__stack_top)
-    );
 }
