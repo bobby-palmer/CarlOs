34c6636e64ebffbf63a4c830a76161d714210d79
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-12 23:50:25 -0400

    entry point

--- build.zig
+++ build.zig
@@ -1,3 +1,26 @@
-fn build() void {
+const std = @import("std");
 
+fn build(b: *std.Build) void {
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .riscv32,
+        .os_tag = .freestanding,
+        .abi = .none
+    });
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const kernel = b.addExecutable(.{
+        .name = "kernel",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    kernel.addCSourceFiles(&.{
+        "kernel/kmain.c",
+    }, &.{
+        "-std=c11", "-g3", "-Wall", "-Wextra",
+        "-fno-stack-protector", "-ffreestanding", "-nostdlib",
+    });
+
+    kernel.setLinkerScript(.{ .path = "kernel/kernel.ld" });
 }
---
+++ kernel/kmain.c
@@ -0,0 +1,14 @@
+void kmain(void) {
+
+}
+
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+void boot(void) {
+  __asm__ __volatile__(
+    "mv sp, %[stack_top]\n" // Set the stack pointer
+    "j kernel_main\n"       // Jump to the kernel main function
+    :
+    : [stack_top] "r" (__stack_top) // Pass the stack top address as %[stack_top]
+  );
+}
