961865869195a138102527b7004ee424b096ae67
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-13 09:04:28 -0400

    yer

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,17 +1,19 @@
-// variables from kernel.ld
 extern char __bss[], __bss_end[], __stack_top[];
 
-void kmain(void) {
+void kmain(void) 
+{
   for (;;);
 }
 
+// Setup stack pointer and jump to kmain
 __attribute__((section(".text.boot")))
 __attribute__((naked))
-void boot(void) {
-  __asm__ __volatile__(
-    "mv sp, %[stack_top]\n"         // Set the stack pointer
-    "j kmain\n"                     // Jump to the kernel main function
-    :
-    : [stack_top] "r" (__stack_top) // Pass the stack top address as %[stack_top]
-  );
+void boot(void) 
+{
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n"
+        "j kmain\n"      
+        :
+        : [stack_top] "r" (__stack_top)
+    );
 }
