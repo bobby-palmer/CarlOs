86d837608b497601dadeee92940be5b1fd7ce7bf
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-17 14:18:30 -0400

    square 1

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,23 +1,5 @@
 #include <stdint.h>
 
-// want to remove duplicate definitions 
-// using extern char with STACK_SZ = &ch
-#define STACK_SZ 0x4000 
-
-extern char __stack_top[], __bss_start[], __bss_end[];
-
-// set stack pointer
-static void init_stack(uint64_t hart_id) 
-{
-  char *stack_top = __stack_top - hart_id * STACK_SZ;
-  asm volatile (
-        "mv sp, %0"  
-        :                 // no output
-        : "r"(stack_top)  // Input: stack address
-        : "sp"            // Clobbered register
-    );
-}
-
 // FLOW:
 // - OpenSBI hart 0 init
 //    - Zero bss
