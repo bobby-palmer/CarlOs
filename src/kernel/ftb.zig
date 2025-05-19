735f18b714209a622a3b2ae33128391e40c0fdb6
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-18 23:27:11 -0700

    find props

--- src/kernel/ftb.zig
+++ src/kernel/ftb.zig
@@ -1,3 +1,9 @@
+const FDT_BEGIN_NODE = 0x00000001;
+const FDT_END_NODE = 0x00000002;
+const FDT_PROP = 0x00000003;
+const FDT_NOP = 0x00000004;
+const FDT_END = 0x00000009;
+
 const DtbHeader = extern struct {
     magic: u32,
     totalsize: u32,
@@ -15,10 +21,44 @@ pub const Dtb = [*]align(@alignOf(DtbHeader)) u8;
 
 const std = @import("std");
 
-pub fn getBootCpuId(dtb: Dtb) u32 {
+// get value of field in native endian
+fn getField(dtb: Dtb, comptime field: []const u8) u32 {
+    if (!@hasField(DtbHeader, field)) {
+        @compileError("DtbHeader has no field named: " ++ field);
+    }
+
     return (
-        std.mem.bigToNative(u32, 
-            @as(*DtbHeader, @ptrCast(dtb)).*.boot_cpuid_phys
+        std.mem.bigToNative(u32,
+            @field(
+                @as(*DtbHeader, @ptrCast(dtb)).*, 
+                field
+            )
         )
     );
+}
+
+pub fn getBootCpuId(dtb: Dtb) u32 {
+    return getField(dtb, "boot_cpuid_phys");
+}
+
+pub fn getPropOffset(dtb: Dtb, prop: []const u8) ?u32 {
+    
+    const strings_off = getField(dtb, "off_dt_strings");
+    const strings_size = getField(dtb, "size_dt_strings");
+
+    var i: u32 = 0;
+
+    while (i < strings_size) {
+        const slice = std.mem.span(
+            @as([*:0]u8, @ptrCast(dtb + strings_off + i))
+        );
+
+        if (std.mem.eql(u8, slice, prop)) {
+            return i;
+        } else {
+            i += @intCast(slice.len + 1);
+        }
+    }
+
+    return null;
 }
