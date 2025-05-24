7ccfa8b727f2b4006ab298baed1adec7ed2823e1
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-05-24 14:09:47 -0700

    find memory node

--- src/kernel/boot.zig
+++ src/kernel/boot.zig
@@ -16,6 +16,8 @@ export fn boot2(_: u64, dtb: [*] const u8) noreturn {
     while (true) {}
 }
 
+const std = @import("std");
+
 fn zeroBss() void {
     const bss_start = @extern([*] u8, .{.name = "__bss_start"});
     const bss_end = @extern([*] u8, .{.name = "__bss_end"});
@@ -25,7 +27,20 @@ fn zeroBss() void {
 }
 
 fn initRam(dtb: [*] const u8) void {
-    _ = @as(* const Fdt, @ptrCast(@alignCast(dtb)));
+    const fdt_header = @as(* const FdtHeader, @ptrCast(@alignCast(dtb)));
+    const tree_size = std.mem.bigToNative(u32, fdt_header.size_dt_struct) 
+        / @sizeOf(u32);
+
+    const tree = @as([*] const u32, @ptrCast(fdt_header)) +
+        @sizeOf(FdtHeader) + std.mem.bigToNative(u32, fdt_header.off_dt_struct); 
+
+    for (0..tree_size) |i| {
+        if (std.mem.bigToNative(u32, tree[i]) == FDT_BEGIN_NODE) {
+            if (std.mem.eql(u8, "memory", fdtGetNodeName(@ptrCast(tree + i + 1)))) {
+                putChar('Y');
+            }
+        }
+    }
 }
 
 const FDT_BEGIN_NODE = 0x00000001;
@@ -34,7 +49,7 @@ const FDT_PROP = 0x00000003;
 const FDT_NOP = 0x00000004;
 const FDT_END = 0x00000009;
 
-const Fdt = extern struct {
+const FdtHeader = extern struct {
     magic: u32,
     totalsize: u32,
     off_dt_struct: u32,
@@ -47,3 +62,26 @@ const Fdt = extern struct {
     size_dt_struct: u32,
 };
 
+fn fdtGetNodeName(nameStart: [*] const u8) [] const u8 {
+    var i: usize = 0;
+
+    while (true) {
+        if (nameStart[i] == '@' or nameStart[i] == 0) {
+            return nameStart[0..i];
+        }
+
+        i += 1;
+    }
+}
+
+// move later this is for debuging
+fn putChar(ch: u8) void {
+    asm volatile(
+        "ecall"
+        :
+        : [ch]     "{a0}" (ch),
+          [id]     "{a6}" (0),
+          [legacy] "{a7}" (1),
+        : "memory", "a0", "a1"
+    );
+}
