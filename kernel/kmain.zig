6498de726d0e4e582a94be03b35ae8597f2838a7
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-10-08 10:25:27 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/kmain.zig
+++ kernel/kmain.zig
@@ -6,5 +6,23 @@ export fn _start() linksection(".text.boot") callconv(.naked) noreturn {
 }
 
 export fn kmain() noreturn {
+    clearBss();
+
+    const sbi = @import("sbi.zig");
+    _ = sbi.debug_print("Hello from kmain\n");
+
     while (true) {}
+}
+
+extern var __bss: u8;
+extern var __bss_end: u8;
+
+fn clearBss() void {
+    
+    const bss_start = @intFromPtr(&__bss);
+    const bss_end = @intFromPtr(&__bss_end);
+    const bss_len = bss_end - bss_start;
+    
+    const bss_slice = @as([*]u8, @ptrFromInt(bss_start))[0..bss_len];
+    @memset(bss_slice, 0);
 }
