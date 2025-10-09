04f3f94333f12044c129c96125401c693e7016a8
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-10-09 15:25:56 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/kmain.zig
+++ kernel/kmain.zig
@@ -6,13 +6,34 @@ export fn _start() linksection(".text.boot") callconv(.naked) noreturn {
     );
 }
 
+const std = @import("std");
+const sbi = @import("sbi.zig");
+
 /// rest of setup for the boot hart
-export fn kmain(_: u64, dtb_bytes: [*]const u8) noreturn {
+export fn kmain(_: u64, _: [*]const u8) noreturn {
     clearBss();
-    while (true) {}
+
+    const buffer = getHeapBuffer();
+    var fa = std.heap.FixedBufferAllocator.init(buffer);
+
+    const allocator = fa.allocator();
+
+    _ = allocator.alloc(u64, 1) catch {
+        _ = sbi.debugPrint("Failed!");
+        stop();
+    };
+
+    _ = sbi.debugPrint("Good!");
+
+    stop();
 }
 
-// TODO make these scoped to the fn below
+fn stop() noreturn {
+    while (true) {
+        asm volatile ("wfi");
+    }
+}
+
 extern var __bss: u8;
 extern var __bss_end: u8;
 
@@ -21,7 +42,21 @@ fn clearBss() void {
     const bss_start = @intFromPtr(&__bss);
     const bss_end = @intFromPtr(&__bss_end);
     const bss_len = bss_end - bss_start;
+
+    if (bss_len == 0) {
+        _ = sbi.debugPrint("Bad len\n");
+    }
     
     const bss_slice = @as([*]u8, @ptrFromInt(bss_start))[0..bss_len];
     @memset(bss_slice, 0);
+}
+
+extern var __early_alloc_start: u8;
+extern var __early_alloc_end: u8;
+
+fn getHeapBuffer() []u8 {
+    const start = @intFromPtr(&__early_alloc_start);
+    const end = @intFromPtr(&__early_alloc_end);
+    const len = end - start;
+    return @as([*]u8, @ptrFromInt(start))[0..len];
 }
