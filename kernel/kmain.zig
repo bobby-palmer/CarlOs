218ff141863c73fe30c4b557befa14366b6e757e
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-10-10 22:17:12 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/kmain.zig
+++ kernel/kmain.zig
@@ -1,4 +1,5 @@
 /// fn for the boot hart to get tmp stack
+/// TODO change this to use static array in zig code instead of linker
 export fn _start() linksection(".text.boot") callconv(.naked) noreturn {
     asm volatile (
         \\ la sp, __stack_top
@@ -10,45 +11,28 @@ const std = @import("std");
 const sbi = @import("sbi.zig");
 const fdt = @import("fdt.zig");
 
+var early_heap: [0x100000] u8 = undefined;
+
 /// rest of setup for the boot hart
 export fn kmain(_: u64, dtb: [*]const u8) noreturn {
-    clearBss();
+    clearBss(); // Must do this first!!!
 
-    var fa = std.heap.FixedBufferAllocator.init(getHeapBuffer());
+    var fa = std.heap.FixedBufferAllocator.init(&early_heap);
     const allocator = fa.allocator();
 
     const device_tree = fdt.Fdt.init(dtb, allocator) catch {
-        _ = sbi.debugPrint("Bad fdt\n");
+        _ = sbi.debugPrint("Allocator failure!\n");
         stop();
     };
 
     if (!device_tree.header.verify()) {
-        _ = sbi.debugPrint("Cannot verify fdt header\n");
+        _ = sbi.debugPrint("Cannot verify fdt header!\n");
         stop();
     }
 
-    printNode(device_tree.root);
-    
-    // NECT STEPS
+    _ = sbi.debugPrint("GOOD!");
 
     stop();
-}
-
-fn printNode(node: fdt.Fdt.StructNode) void {
-    _ = sbi.debugPrint(node.name);
-    _ = sbi.debugPrint("\n");
-    _ = sbi.debugPrint("Props:\n");
-
-    for (node.props.items) |prop| {
-        _ = sbi.debugPrint(prop.name);
-        _ = sbi.debugPrint(": ");
-        _ = sbi.debugPrint(prop.value);
-        _ = sbi.debugPrint("\n");
-    }
-
-    for (node.sub_nodes.items) |sub_node| {
-        printNode(sub_node);
-    }
 }
 
 /// Halt cpu
@@ -58,7 +42,6 @@ fn stop() noreturn {
     }
 }
 
-// TODO learn about this!
 extern var __bss: u8;
 extern var __bss_end: u8;
 
@@ -71,12 +54,3 @@ fn clearBss() void {
     @memset(bss_slice, 0);
 }
 
-extern var __early_alloc_start: u8;
-extern var __early_alloc_end: u8;
-
-fn getHeapBuffer() []u8 {
-    const start = @intFromPtr(&__early_alloc_start);
-    const end = @intFromPtr(&__early_alloc_end);
-    const len = end - start;
-    return @as([*]u8, @ptrFromInt(start))[0..len];
-}
