a943bf341e34883d95c0a15cbfbdaad0d764098b
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-10-20 11:14:25 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

---
+++ kernel/constants.zig
@@ -0,0 +1,2 @@
+pub const PAGE_SHIFT: u32 = 12; // log2(4096)
+pub const PAGE_SIZE: usize = 1 << PAGE_SHIFT; // 4KB
--- kernel/vmm.zig
+++ kernel/vmm.zig
@@ -1,6 +1,46 @@
-// Virtual memory manager
+//! Virtual memory manager (vmm) module
 
 const std = @import("std");
+
+/// Permission flags for a virtual page
+/// excluding flags that are automatically set
+pub const Flags = packed struct {
+    /// Read bit. If this bit is 1, then we are permitted to read from this
+    /// memory address. Otherwise, the MMU will cause a page fault on
+    /// loads.
+    R: u1,
+    /// Write bit. If this bit is 1, then we are permitted to write to this
+    /// memory address. Otherwise, the MMU will cause a page fault on
+    /// stores.
+    W: u1,
+    /// Execute bit. If this bit is 1, then the CPU is permitted to execute
+    /// an instruction fetch (IF) at this memory address. Otherwise, the
+    /// MMU will cause a page fault on IF.
+    X: u1,
+    /// User bit. If this bit is 1, then a user-application is permitted to
+    /// RWX (depending on the bits above) at this memory location. If this
+    /// bit is 0, the only the operating system can RWX at this memory
+    /// location.
+    U: u1,
+    /// Global bit. This memory address is used for more than one
+    /// application, so this is a hint to the cache policy. In other words,
+    /// don’t evict it when we switch programs.
+    G: u1,
+
+    /// Autofill flags that should always have the same value
+    fn toFullFlags(self: *const @This()) Sv39Pte.Flags {
+        return Sv39Pte.Flags {
+            .V = 1,
+            .R = self.R,
+            .W = self.W,
+            .X = self.X,
+            .U = self.U,
+            .G = self.G,
+            .A = 1,
+            .D = 0,
+        };
+    }
+};
 
 /// Page table entry in Sv39 paging for RISC-V 64
 const Sv39Pte = packed struct {
@@ -37,7 +77,7 @@ const Sv39Pte = packed struct {
         D: u1,
     };
 
-    flags: Flags,
+    flags: @This().Flags,
     /// Reserved for Software. The operating system can put whatever it wants
     /// in here.
     reserved0: u2,
@@ -48,5 +88,11 @@ const Sv39Pte = packed struct {
     reserved1: u10,
 };
 
-// Assert bit packing is correct in size
-const _ = std.debug.assert(@sizeOf(Sv39Pte) == @sizeOf(u64));
+
+const PageTable = struct {
+    entries: [512]Sv39Pte
+};
+
+pub fn mapPage(base_table: usize, vaddr: usize, paddr: usize, flags: Flags) void {
+    const page_table: *PageTable = @ptrFromInt(base_table);
+}
