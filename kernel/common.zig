e4186c52231387585490465daa491e4833ca14aa
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-10-21 08:17:23 -0400

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/common.zig
+++ kernel/common.zig
@@ -1,2 +1,12 @@
 pub const PAGE_SHIFT: u32 = 12; // log2(4096)
 pub const PAGE_SIZE: usize = 1 << PAGE_SHIFT; // 4KB
+
+/// Return first page number that starts on or before addr
+pub fn pageDown(addr: usize) u64 {
+    return addr >> PAGE_SHIFT;
+}
+
+/// return first page that starts on or after addr
+pub fn pageUp(addr: usize) u64 {
+    return (addr + PAGE_SIZE - 1) / PAGE_SIZE;
+}
