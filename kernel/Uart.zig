2f19079ffd2df07683d84465e0be1d3788ab23e1
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-11-03 15:32:46 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/Uart.zig
+++ kernel/Uart.zig
@@ -1,3 +1,11 @@
-//! Driver for outputting to generic uart device
+const std = @import("std");
+
+base_addr: usize,
+irq_num: i32,
+
+
+const Ns16550 = struct {
+
+};
 
 
---
+++ kernel/ringbuf.zig
@@ -0,0 +1,1 @@
+//! Fixed size ring buffer module
