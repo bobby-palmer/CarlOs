8342f5549261625be187c4e8724f5dff88f63236
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-11-04 14:18:10 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/common/MemorySpan.zig
+++ kernel/common/MemorySpan.zig
@@ -1,6 +1,15 @@
+//! Simple struct to represent a range of addresses
+
+const MemorySpan = @This();
+
 start: usize,
 end: usize,
 
-pub fn len(self: *const @This()) usize {
+pub fn len(self: *const MemorySpan) usize {
     return self.end - self.start;
+}
+
+/// Slice over the bytes in the region
+pub fn slice(self: *const MemorySpan) []u8 {
+    return @as([*]u8, @ptrFromInt(self.start))[0..self.len()];
 }
