ba0fb463b07a0e43d25677db88d47ded23b86c54
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-11-05 08:39:49 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/pmm.zig
+++ kernel/pmm.zig
@@ -2,39 +2,60 @@
 
 const std = @import("std");
 const common = @import("common.zig");
+const constants = common.constants;
 
-/// Initialize phyical memory manager at boot. Should be called exactly once
+pub const Paddr = struct {
+    paddr: usize,
+
+    /// Return kernel linear map vaddr for a given paddr
+    pub fn getVaddr(self: *const Paddr) usize {
+        return self.paddr + kernel_base;
+    }
+
+    /// Set the base of the kernel linear mapping (should be called once at boot)
+    pub fn setKernelBase(base_addr: usize) void {
+        kernel_base = base_addr;
+    }
+
+    /// Offset to the start of kernel linear mapping
+    var kernel_base: usize = 0;
+};
+
+/// Initialize phyical memory manager at boot. Should be called exactly once.
+/// start and end addr should be phyical addresses
 pub fn init(start_addr: usize, end_addr: usize) void {
     const start_page_addr = 
-        std.mem.alignForward(usize, start_addr, common.constants.PAGE_SIZE); 
+        std.mem.alignForward(usize, start_addr, constants.PAGE_SIZE); 
 
     const end_page_addr =
-        std.mem.alignBackward(usize, end_addr, common.constants.PAGE_SIZE);
+        std.mem.alignBackward(usize, end_addr, constants.PAGE_SIZE);
 
     var current_page_addr = start_page_addr;
 
     while (current_page_addr < end_page_addr) 
-        : (current_page_addr += common.constants.PAGE_SIZE) {
+        : (current_page_addr += constants.PAGE_SIZE) {
         freePage(current_page_addr);
     }
 }
 
-/// Try to allocate a page a return its start address. 
-/// Page is not guaranteed to be zeroed
-pub fn allocPage() error{OutOfMemory}!usize {
+/// Try to allocate a page a return its phyical start address. Page is not
+/// guaranteed to be zeroed
+pub fn allocPage() error{OutOfMemory}!Paddr {
     lock.lock();
     defer lock.unlock();
 
-    if (free_pages.popFirst()) |node| {
-        return @intFromPtr(node);
+    if (free_pages) |paddr| {
+        const vaddr = paddr.getVaddr();
+        free_pages = @as(*Node, @ptrFromInt(vaddr)).*;
+        return paddr;
     } else {
         return error.OutOfMemory;
     }
 }
 
-/// Free a page starting at 'start_addr'
-pub fn freePage(start_addr: usize) void {
-    if (!std.mem.isAligned(start_addr, common.constants.PAGE_SIZE)) {
+/// Free a page starting at phyical address 'start_addr'
+pub fn freePage(start_addr: Paddr) void {
+    if (!std.mem.isAligned(start_addr.paddr, constants.PAGE_SIZE)) {
         @panic("freePage(...) called on missaligned address");
     }
 
@@ -42,11 +63,13 @@ pub fn freePage(start_addr: usize) void {
     defer lock.unlock();
 
     const node = 
-        @as(*std.SinglyLinkedList.Node, @ptrFromInt(start_addr));
+        @as(*Node, @ptrFromInt(start_addr.getVaddr()));
 
-    node.* = .{};
-    free_pages.prepend(node);
+    node.* = free_pages;
+    free_pages = node;
 }
 
 var lock = common.Spinlock{};
-var free_pages = std.SinglyLinkedList{};
+
+const Node = ?Paddr;
+var free_pages: Node = null;
---
+++ kernel/vmm.zig
@@ -0,0 +1,55 @@
+//! Riscv sv39 virtual paging handler. Following
+//! https://marz.utk.edu/my-courses/cosc130/lectures/virtual-memory/
+
+const pmm = @import("pmm.zig");
+const common = @import("common.zig");
+const constants = common.constants;
+
+/// Permission flags for virtual memory mapping
+pub const Flags = packed struct {
+    /// Valid bit. If this is 0, then the entire entry is invalid. This will
+    /// cause the MMU to throw a page fault.
+    V: u1 = 1,
+    /// Read bit. If this bit is 1, then we are permitted to read from this
+    /// memory address. Otherwise, the MMU will cause a page fault on loads.
+    R: u1 = 0,
+    /// Write bit. If this bit is 1, then we are permitted to write to this
+    /// memory address. Otherwise, the MMU will cause a page fault on stores.
+    W: u1 = 0,
+    /// Execute bit. If this bit is 1, then the CPU is permitted to execute an
+    /// instruction fetch (IF) at this memory address. Otherwise, the MMU will
+    /// cause a page fault on IF.
+    X: u1 = 0,
+    /// User bit. If this bit is 1, then a user-application is permitted to RWX
+    /// (depending on the bits above) at this memory location. If this bit is
+    /// 0, the only the operating system can RWX at this memory location.
+    U: u1 = 0,
+    /// Global bit. This memory address is used for more than one application,
+    /// so this is a hint to the cache policy. In other words, don’t evict it
+    /// when we switch programs.
+    G: u1 = 0,
+    /// Accessed bit. Automatically set by the CPU whenever this memory address
+    /// is accessed (IF, load, or store).
+    A: u1 = 0,
+    /// Dirty bit. Automatically set by the CPU whenever this memory address is
+    /// written to (stores).
+    D: u1 = 0,
+};
+
+/// Sv39 page table entry
+const Pte = packed struct {
+    flags: Flags,
+    rsw: u2,
+    ppn: u44,
+    rsw2: u10,
+};
+
+const PageTable = struct {
+    entries: [512]Pte,
+};
+
+const PageTablePointer = pmm.Paddr;
+
+fn vpn(vaddr: usize, level: u8) usize {
+    return 0x1FF & (vaddr >> (12 + 9 * level));
+}
